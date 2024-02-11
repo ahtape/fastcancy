a3a8b49946eae2131c0d82e3a1c10f27e4600368
ahtape <athasi.kurus@gmail.com>
2024-02-11 08:16:39 +0700

    add explore page and router module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,22 @@ import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import {MatIconModule} from '@angular/material/icon';
 import { NavbarComponent } from './navbar/navbar.component';
-import { PostJobComponent } from './post-job/post-job.component';
-import { FindJobComponent } from './find-job/find-job.component';
-import { JobListComponent } from './find-job/job-list/job-list.component';
-import { PostJobCreateComponent } from './post-job-create/post-job-create.component';
+import { PostJobComponent } from './explore-page/post-job/post-job.component';
+import { FindJobComponent } from './explore-page/find-job/find-job.component';
+import { JobListComponent } from './explore-page/find-job/job-list/job-list.component';
+import { PostJobCreateComponent } from './explore-page/post-job-create/post-job-create.component';
 import { JobPageComponent } from './job-page/job-page.component';
 import { CreatedJobComponent } from './job-page/created-job/created-job.component';
 import { AppliedJobComponent } from './job-page/applied-job/applied-job.component';
 import { JobDescriptionComponent } from './job-page/job-description/job-description.component';
 import { ApplicantProfileComponent } from './job-page/applicant-profile/applicant-profile.component';
 import { AppliedJobDescriptionComponent } from './job-page/applied-job-description/applied-job-description.component';
+import { ExplorePageComponent } from './explore-page/explore-page.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', component: ExplorePageComponent},
+];
 
 @NgModule({
   declarations: [
@@ -31,12 +37,14 @@ import { AppliedJobDescriptionComponent } from './job-page/applied-job-descripti
     AppliedJobComponent,
     JobDescriptionComponent,
     ApplicantProfileComponent,
-    AppliedJobDescriptionComponent
+    AppliedJobDescriptionComponent,
+    ExplorePageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatIconModule
+    MatIconModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
