00a53f3b3f51b8793e7b27bc07b10d6d7e64a311
ahtape <athasi.kurus@gmail.com>
2024-02-17 10:52:22 +0700

    add env

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        "projectId":"fastcancy",
+        "appId":"1:724925589736:web:cc6a12d90ba6995bde815d",
+        "storageBucket":"fastcancy.appspot.com",
+        "apiKey":"AIzaSyBYWINGHptQE2-8Lu6KDZCAF6yN4RDKjvc",
+        "authDomain":"fastcancy.firebaseapp.com",
+        "messagingSenderId":"724925589736",
+        "measurementId":"G-WHHY79MEMJ"
+      }
+};
